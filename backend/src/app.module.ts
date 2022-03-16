d0bdceee9815b5166aa2cc9d845015ed03595bdb
kondo0602 <kondo8363@gmail.com>
2022-03-16 23:02:40 +0900

    remove: sampleファイルを削除

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common'
 import { ParticipantController } from './controller/participant.controller'
-import { SampleController } from './controller/sample/some-data.controller'
 
 // memo: DIコンテナとしては使わないため、controllerの追加だけしてください
 @Module({
   imports: [],
-  controllers: [SampleController, ParticipantController],
+  controllers: [ParticipantController],
   providers: [],
 })
 export class AppModule {}

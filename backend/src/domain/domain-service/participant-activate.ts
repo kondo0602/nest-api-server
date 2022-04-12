6176e3eb1357681dad0bd58e35931d20ea72c915
kondo0602 <kondo8363@gmail.com>
2022-04-12 09:39:27 +0900

    feat: 参加者が増えた場合、参加先のペアが自動的に選択されるように修正

--- backend/src/domain/domain-service/participant-activate.ts
+++ backend/src/domain/domain-service/participant-activate.ts
@@ -32,11 +32,20 @@ export class ParticipantActivate {
     })
 
     // TODO: 最も参加人数が少ないチームの中で、最も参加人数が少ないペアから自動的に自動敵に参加先が選択されるように修正
-    // TODO: ペアへの参加によってペアが4名になってしまう場合、自動的に2つのペアに分解されるように修正
-    const targetTeam = await this.participantRepo.getTeamByTeamId('1')
-    const targetPair = targetTeam.getPairByPairId('1')
 
+    // 1. repositoryで最も参加人数が少ないチームを取得する
+    const targetTeam = await this.participantRepo.getTeamWithFewestParticipants()
+
+    // 2. 取得したチームが公開しているメソッドで最も参加人数の少ないペアを取得する
+    const targetPair = targetTeam.getPairWithFewestParticipants()
+
+    // 3. そのペアに参加者をaddして永続化する
     targetPair.addParticipant(participant)
+
+    // const targetTeam = await this.participantRepo.getTeamByTeamId('1')
+    // const targetPair = targetTeam.getPairByPairId('1')
+
+    // TODO: ペアへの参加によってペアが4名になってしまう場合、自動的に2つのペアに分解されるように修正
     targetTeam.removePair(targetPair.getId())
     targetTeam.addPair(targetPair)
 

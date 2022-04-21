5ae37c3f4fe6e8602b2de00cd253ef02d60e9110
kondo0602 <kondo8363@gmail.com>
2022-04-21 13:56:50 +0900

    feat: 独自のエラーオブジェクト（404:Not Found）を追加

--- backend/src/domain/__shared__/exception/domain-exception.ts
+++ backend/src/domain/__shared__/exception/domain-exception.ts
@@ -1,7 +1,14 @@
-import { BadRequestException } from '@nestjs/common'
+import { BadRequestException, NotFoundException } from '@nestjs/common'
 
 // 400 Bad Request
 export class DomainBadRequestException extends BadRequestException {
+  constructor(message: string) {
+    super(message)
+  }
+}
+
+// 404 Not Found
+export class DomainNotFoundException extends NotFoundException {
   constructor(message: string) {
     super(message)
   }

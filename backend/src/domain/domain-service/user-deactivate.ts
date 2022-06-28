d5b549348d4ddfe60d98a8cb83d64096899d8f73
kondo0602 <kondo8363@gmail.com>
2022-06-28 09:47:23 +0900

    fix: getUserByUserId()でユーザがいない時に例外を発生させるのではなく、nullを返すように修正

--- backend/src/domain/domain-service/user-deactivate.ts
+++ backend/src/domain/domain-service/user-deactivate.ts
@@ -37,7 +37,7 @@ export class UserDeactivate {
       if (fewestPair.getUserCount() >= Pair.MAXIMUM_NUMBER_OF_PARTICIPANTS) {
         targetTeam.addPair(targetPair)
         // TODO: コンソール出力 -> 管理者へのメール送信に処理を変更する
-        console.log(`${targetUser.getName()}さんが抜けました.`)
+        console.log(`${targetUser!.getName()}さんが抜けました.`)
         // 休会/退会によってペアが残された参加者1名になってしまう場合
       } else if (
         targetPair.getUserCount() < Pair.MINIIMUM_NUMBER_OF_PARTICIPANTS
@@ -60,16 +60,16 @@ export class UserDeactivate {
     if (targetTeam.getUserCount() <= Team.MINIIMUM_NUMBER_OF_PARTICIPANTS) {
       // TODO: コンソール出力 -> 管理者へのメール送信に処理を変更する
       console.log(
-        `${targetUser.getName()}さんが抜けたことによって、チーム${targetTeam.getName()}が${targetTeam.getUserCount()}名になりました.`,
+        `${targetUser!.getName()}さんが抜けたことによって、チーム${targetTeam.getName()}が${targetTeam.getUserCount()}名になりました.`,
       )
     }
 
     this.userRepo.updateTeam(targetTeam)
 
     const removedUser = new RemovedUser({
-      id: targetUser.getId(),
-      name: targetUser.getName(),
-      email: targetUser.getEmail(),
+      id: targetUser!.getId(),
+      name: targetUser!.getName(),
+      email: targetUser!.getEmail(),
       statusId: statusId,
     })
 

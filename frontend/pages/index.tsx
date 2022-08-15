49be5caff9e641cc19d10a9116990f43669583f5
kondo0602 <kondo8363@gmail.com>
2022-08-16 08:10:57 +0900

    style: indexページのスタイル修正

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,4 @@
-import type { NextPage } from "next";
-import { setRevalidateHeaders } from "next/dist/server/send-payload";
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 type Users = {
@@ -40,60 +37,20 @@ const Home = (props: Users) => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Praha Challenge!</a>
+          Welcome to <a href="#">Praha Challenge!</a>
         </h1>
 
-        {props.user.map((user) => (
-          <div key={user.id}>
-            <p>{`Name: ${user.name}`}</p>
-            <p>{`Email: ${user.email}`}</p>
-            <p>{`Status: ${user.statusId}`}</p>
-          </div>
-        ))}
-
+        <h2>Member List</h2>
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {props.user.map((user) => (
+            <a key={user.id} href="#" className={styles.card}>
+              <h2>{user.name}</h2>
+              <p>{user.email}</p>
+              <p>{`Status: ${user.statusId}`}</p>
+            </a>
+          ))}
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };

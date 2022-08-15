fb3f969cb07d837b3f801f795ebada9c67f8e20a
kondo0602 <kondo8363@gmail.com>
2022-08-15 18:26:39 +0900

    fix: 認証をメールアドレスで行うように修正

--- frontend/pages/sign-in.tsx
+++ frontend/pages/sign-in.tsx
@@ -22,19 +22,12 @@ const uiConfig = {
   // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
   signInSuccessUrl: "/",
   // We will display Google and Facebook as auth providers.
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-  ],
+  signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
 };
 
 function SignInScreen() {
   return (
-    <div>
-      <h1>My App</h1>
-      <p>Please sign-in:</p>
-      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
-    </div>
+    <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
   );
 }
 

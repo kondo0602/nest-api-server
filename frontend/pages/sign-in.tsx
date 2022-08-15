9077b3a67550ff38e0c5745bfc86004307b05b89
kondo0602 <kondo8363@gmail.com>
2022-08-16 06:40:21 +0900

    feat: ユーザのログイン状態を判定するロジックを追加

--- frontend/pages/sign-in.tsx
+++ frontend/pages/sign-in.tsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 import SignInForm from "../components/sign-in-form";
 
 import "../utils/firebase/init";
 
 const SignInPage = () => {
-  return <SignInForm />;
+  const [userStatus, setUserStatus] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(getAuth(), (user) => {
+      user ? setUserStatus(true) : setUserStatus(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (userStatus) {
+    // TODO: バックエンドのユーザ一覧を表示するように修正
+    return <p>ログイン中</p>;
+  } else {
+    return <SignInForm />;
+  }
 };
 
 export default SignInPage;
